f016cd7e564ce2929a73c8b282455da1b2436376
kimtaeoug <tkdldjs312@naver.com>
2020-08-14 12:56:07 +0900

    뷰페이저와 하단 탭 연결까지 끝~~~

--- app/src/main/java/com/example/uitest/MainActivity.kt
+++ app/src/main/java/com/example/uitest/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.uitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomnavi = findViewById<View>(R.id.bottomnavi) as BottomNavigationView
+        bottomnavi.setOnNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(p0:MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.aitem ->{
+                val fragmenta = ScreenSlidePageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentlocation,fragmenta).commit()
+            }
+            R.id.bitem ->{
+                val fragmentb = testfragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentlocation,fragmentb).commit()
+            }
+            R.id.citem ->{
+                val fragmentc = ScreenSlidePageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentlocation,fragmentc).commit()
+            }
+            R.id.ditem ->{
+                val fragmentd = testfragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentlocation,fragmentd).commit()
+            }
+            R.id.eitem ->{
+                val fragmente = ScreenSlidePageFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentlocation,fragmente).commit()
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/uitest/ScreenSlidePagerActivity.kt
+++ app/src/main/java/com/example/uitest/ScreenSlidePagerActivity.kt
@@ -2,15 +2,19 @@ package com.example.uitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-//프래그먼트 수 
+//프래그먼트 수
 private const val NUM_PAGES = 5
 
-class ScreenSlidePagerActivity : FragmentActivity() {
+class ScreenSlidePagerActivity : FragmentActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     /**
      * The pager widget, which handles animation and allows swiping horizontally to access previous
@@ -28,8 +32,14 @@ class ScreenSlidePagerActivity : FragmentActivity() {
 
         // The pager adapter, which provides the pages to the view pager widget.
         val pagerAdapter = ScreenSlidePagerAdapter(this)
+        //viewpager에 어댑터 연결
         viewPager.adapter = pagerAdapter
+        //viewpager와 bottom navi를 연결하는 함수
+        viewPager.registerOnPageChangeCallback(PageChangeCallback())
+        //viewpager animation
         viewPager.setPageTransformer(ZoomOutPageTransformer())
+        //bottom navigation
+        bottomnavi.setOnNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
@@ -54,7 +64,50 @@ class ScreenSlidePagerActivity : FragmentActivity() {
             return when(position){
                 0 -> ScreenSlidePageFragment()
                 1 -> testfragment()
+                2 -> ScreenSlidePageFragment()
+                3 -> testfragment()
+                4 -> ScreenSlidePageFragment()
                 else-> ScreenSlidePageFragment()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(p0:MenuItem): Boolean {
+        when(p0.itemId){
+            R.id.aitem -> {
+                viewPager.currentItem = 0
+                return true
+            }
+            R.id.bitem -> {
+                viewPager.currentItem = 1
+                return true
+            }
+            R.id.citem -> {
+                viewPager.currentItem = 2
+                return true
+            }
+            R.id.ditem -> {
+                viewPager.currentItem = 3
+                return true
+            }
+            R.id.eitem -> {
+                viewPager.currentItem = 4
+                return true
+            }
+        }
+        return true
+    }
+    //
+    private inner class PageChangeCallback: ViewPager2.OnPageChangeCallback(){
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            bottomnavi.selectedItemId = when(position){
+                0 -> R.id.aitem
+                1 -> R.id.bitem
+                2 -> R.id.citem
+                3 -> R.id.ditem
+                4 -> R.id.eitem
+                else-> R.id.aitem
             }
         }
     }
