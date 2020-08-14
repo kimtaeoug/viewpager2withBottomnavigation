9a83ca106fa02d82b0ec5238f44be912ed0a729e
kimtaeoug <tkdldjs312@naver.com>
2020-08-14 12:07:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uitest/ScreenSlidePageFragment.kt
@@ -0,0 +1,15 @@
+package com.example.uitest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+}
